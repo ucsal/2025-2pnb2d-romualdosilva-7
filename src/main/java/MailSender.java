2e00da57a9009a09311004d4d65a7bfbba456c40
romualdosilva-7 <romualdo.silva@ucsal.edu.br>
2025-09-11 17:42:51 -0300

    subindo atividade

---
+++ src/main/java/MailSender.java
@@ -0,0 +1,3 @@
+public interface MailSender {
+    void send(String to, String subject, String body);
+}
--- src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
+++ src/main/java/br/com/mariojp/solid/dip/EmailNotifier.java
@@ -1,12 +1,13 @@
 package br.com.mariojp.solid.dip;
 
 public class EmailNotifier {
-	private final SmtpClient smtp = new SmtpClient();
+    private final MailSender mailSender;
 
-	public void welcome(User user) {
-		if ("true".equalsIgnoreCase(System.getProperty("DRY_RUN"))) {
-			// Estado inicial: ainda assim usa SMTP real (bug proposital)
-		}
-		smtp.send(user.email(), "Bem-vindo", "Olá " + user.name());
-	}
+    public EmailNotifier(MailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void welcome(User user) {
+        mailSender.send(user.email(), "Bem-vindo", "Olá " + user.name());
+    }
 }
